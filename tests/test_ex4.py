a9953b74ce62e4799083605c0ff88b2c136e3cd3
fouadsan <F.benayad95@gmail.com>
2022-03-14 18:14:36 +0100

    Factory boy and Faker

--- tests/test_ex4.py
+++ tests/test_ex4.py
@@ -1,4 +1,5 @@
-
+import pytest
+from django.contrib.auth.models import User
 
 # @pytest.fixture()
 # def user_1(db):
@@ -8,21 +9,32 @@
 # def test_set_ckeck_password(user_1):
 #     user_1.set_password("new-password")
 #     assert user_1.check_password("new-password") is True
-    
+
 
 # def test_set_ckeck_password1(user_1):
 #     print('check-user1')
 #     assert user_1.username == "test-user"
-    
+
 # def test_set_ckeck_passwor2(user_1):
 #     print('check-user2')
 #     assert user_1.username == "test-user"
-    
-    
-def test_new_user1(new_user1):
-    print(new_user1.first_name)
-    assert new_user1.first_name == "myName"
-    
-def test_new_user2(new_user2):
-    print(new_user2.is_staff)
-    assert new_user2.is_staff == True
+
+
+# def test_new_user1(new_user1):
+#     print(new_user1.first_name)
+#     assert new_user1.first_name == "myName"
+
+# def test_new_user2(new_user2):
+#     print(new_user2.is_staff)
+#     assert new_user2.is_staff
+
+# @pytest.mark.django_db
+# def test_new_user2(new_user2):
+#     print(new_user2.username)
+#     assert True
+
+
+def test_product(db, product_factory):
+    product = product_factory.create()
+    print(product.description)
+    assert True
